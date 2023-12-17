6872f3186531bb7d53ed9d5751b2443b0ff3653d
Domenico Tenace <domenicotenace99@gmail.com>
2023-12-17 15:43:37 +0100

    clear all files

--- modules/ES6/modules.js
+++
@@ -1,4 +0,0 @@
-import { modPeople } from "./people";
-
-console.log(modPeople); //undefined se il modulo non viene nell'altro file esportato
-//console.log(people); // errore perchè non dichiarata nel file
--- modules/people.js
+++
@@ -1,6 +0,0 @@
-const people = ["Mario", "Luigi", "Toad", "Yoshi"];
-const ages = [12, 23, 45, 56];
-
-console.log(people);
-
-module.exports = { people, ages }; //questo è il valore di ritorno dell'importazione
